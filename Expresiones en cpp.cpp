da3ceead799b52f641401aaf508dbadb28a759f5
berkley7812 <163479460+berkley7812@users.noreply.github.com>
2024-04-10 22:53:05 -0400

    Add files via upload

---
+++ Expresiones en cpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main() {
+	
+	int a, b, c;
+
+	a = 10;
+	b = 5;
+
+	
+	c = b; // c = 5
+    b = a; // b = 10
+	a = c; // a = 5
+
+	std::cout << //variable
+
+
+
+	return 0;
+}
