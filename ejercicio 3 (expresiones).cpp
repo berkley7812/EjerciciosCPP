3744805ac5e6ca6b3169ba53b3d0d27539b43e71
berkley7812 <163479460+berkley7812@users.noreply.github.com>
2024-04-10 22:00:54 -0400

    Update and rename Expresiones en cpp.cpp to ejercicio 3 (expresiones).cpp

--- ejercicio 3 (expresiones).cpp
+++ ejercicio 3 (expresiones).cpp
@@ -3,18 +3,18 @@ int main() {
 	
 	float a, b, c, d, e, f;
 	
-	std::cout << "Ingrese dígito de A: "; std::cin >> a;
+	std::cout << "Ingrese dÃ­gito de A: "; std::cin >> a;
 	std::cout << "B: "; std::cin >> b;
 	std::cout << "C: "; std::cin >> c;
 	std::cout << "D: "; std::cin >> d;
-    std::cout << "E: "; std::cin >> e;
+        std::cout << "E: "; std::cin >> e;
 	std::cout << "F: "; std::cin >> f;
      
 	float resultado;
 
 	resultado = (a + (b / c)) / (d + (e / f));
 	cout.precision(2)
-	std::cout << "\nEl resultado de la operación es: " << resultado << std::endl;
+	std::cout << "\nEl resultado de la operaciÃ³n es: " << resultado << std::endl;
 
 	return 0;
 }
